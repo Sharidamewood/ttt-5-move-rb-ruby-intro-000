c4b5155702cca2d7c3cfcb055d99a157846f4433
Shari Damewood <sharidamewood3@gmail.com>
2020-05-27 02:19:58 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,8 +4,8 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, input, value = "X")
-  board[input] = value
+def move(board, index, value = "X")
+  board[index] = value
 end
 
 def display_board(board)
